755f14b41a2865b4235ea8dca3b24af19e37ab6d
Viet Nguyen (Henry) <49942312+viettask@users.noreply.github.com>
2022-12-10 16:12:07 +1000

    Add files via upload

---
+++ b5_concatingArray.dart
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/concatenation-of-array/
+
+class Solution {
+  List<int> getConcatenation(List<int> nums) {
+      List<int> result = [...nums,...nums];
+      return result;
+  }
+}
