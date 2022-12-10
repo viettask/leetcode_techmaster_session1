7d5558eff135004373af2e8e16b9bd6c16c8b020
Viet Nguyen (Henry) <49942312+viettask@users.noreply.github.com>
2022-12-10 15:21:20 +1000

    Add files via upload

--- b3_IpAddress_leetcode.dart
+++ b3_IpAddress_leetcode.dart
@@ -1,13 +1,10 @@
-//https://leetcode.com/problems/defanging-an-ip-address/description/ 
-
+//https://leetcode.com/problems/defanging-an-ip-address/description/
 
 String defangIPaddr(String address) {
-        return address.replaceAll('.', '[.]');
-    }
-
+  return address.replaceAll('.', '[.]');
+}
 
 void main() {
   print(defangIPaddr("1.1.1.1"));
- print(defangIPaddr("255.100.50.0"));
- 
+  print(defangIPaddr("255.100.50.0"));
 }
